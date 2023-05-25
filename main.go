053b42df929035211b199e8552698451dbeac2d6
Kien Nguyen <kien.nguyen@fundingsocieties.com>
2023-05-25 16:49:28 +0700

    A

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"github.com/kien-fsmk/kbts-hackathon/server"
 	"os"
 	"os/signal"
 	"strings"
+
+	"github.com/kien-fsmk/kbts-hackathon/server"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
@@ -40,6 +41,36 @@ func init() {
 	if err != nil {
 		logger.WithError(err).Fatalf("error loading configuration \n")
 	}
+}
+
+type Payment struct {
+	PaymentID   string    `json:"payment_id"`
+	Amount      int       `json:"amount"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   string    `json:"created_at"`
+	Customer    Customer  `json:"customer"`
+	Recipient   Recipient `json:"recipient"`
+}
+
+type Customer struct {
+	CustomerID string `json:"customer_id"`
+	Email      string `json:"email"`
+}
+
+type Recipient struct {
+	RecipientID             string      `json:"recipient_id"`
+	Name                    string      `json:"name"`
+	Email                   string      `json:"email"`
+	BusinessRegistrationNum string      `json:"business_registration_number"`
+	BankAccount             BankAccount `json:"bank_account"`
+}
+
+type BankAccount struct {
+	BankName    string `json:"bank_name"`
+	AccountNo   string `json:"account_no"`
+	AccountName string `json:"account_name"`
+	Country     string `json:"country"`
 }
 
 // Starting a http server
